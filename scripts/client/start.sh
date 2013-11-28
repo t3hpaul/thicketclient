4c57ea1069b6c121a8332a1c866f130f3c4a0e87
Paul Stasiuk <pstasiuk@Pauls-MacBook-Air.local>
2013-11-27 19:36:25 -0600

    fixing the init script.

--- scripts/client/start.sh
+++ scripts/client/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python /etc/thicketclient/client/client_main_event.py <&- 1>/dev/null 2>&1 &
+python /etc/thicketclient/client_main_event.py <&- 1>/dev/null 2>&1 &
 pid=$!
 echo ${pid} > /var/run/thicketclient.pid
 echo "the pid is" $pid
